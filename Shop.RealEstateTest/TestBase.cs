d01f922280b67554389da2dae1d1e042c099c57a
Opilane TTHK <andero.magi.420+schoolgit@gmail.com>
2024-10-15 15:05:16 +0300

    Tests project

---
+++ Shop.RealEstateTest/TestBase.cs
@@ -0,0 +1,31 @@
+﻿namespace Shop.RealEstateTest;
+
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Core.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class TestBase
+{
+    protected IServiceProvider ServiceProvider { get; set; }
+
+    protected TestBase()
+    {
+        var services = new ServiceCollection();
+        SetupServices(services);
+        ServiceProvider = services.BuildServiceProvider();
+    }
+
+    protected T Svc<T>()
+    {
+        return ServiceProvider.GetService<T>();
+    }
+
+    public virtual void SetupServices(IServiceCollection services)
+    {
+
+    }
+}
