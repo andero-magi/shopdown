a6771029cefec3b2ee0f53152346bcf8da4c49fe
Opilane TTHK <andero.magi.420+schoolgit@gmail.com>
2024-10-21 12:44:22 +0300

    Update RealEstateTest.cs

--- Shop.RealEstateTest/RealEstateTest.cs
+++ Shop.RealEstateTest/RealEstateTest.cs
@@ -1,5 +1,6 @@
 namespace Shop.RealEstateTest;
 
+using Shop.Core.Domain;
 using Shop.Core.Dto;
 using Shop.Core.ServiceInterface;
 
@@ -45,4 +46,54 @@ public class RealEstateTest: TestBase
         Assert.NotNull(b);
     }
 
+    [Fact]
+    public async Task Should_GetByIdRealEstate_WhenReturnsNotEqual()
+    {
+
+    }
+
+    [Fact]
+    public async Task Should_DeleteByIdRealEstate_WhenDeleteRealEstate()
+    {
+        RealEstateDto dto = MockRealEstateData();
+        var svr = Svc<IRealEstateService>();
+
+        RealEstate estate = await svr.Create(dto);
+        Guid id = estate.Id;
+
+        var removed = await svr.Delete(id);
+
+        var found = await svr.GetAsync(id);
+
+        Assert.Null(found);
+        Assert.NotNull(removed);
+    }
+
+    [Fact]
+    public async Task ShouldNot_DeleteByIdRealEstate_WhenDidNotDeleteRealEstate()
+    {
+        RealEstateDto dto = MockRealEstateData();
+
+        var s = Svc<IRealEstateService>();
+
+        var r1 = await s.Create(dto);
+        var r2 = await s.Create(dto);
+
+        var result = await s.Delete(r2.Id);
+
+        Assert.NotEqual(result.Id, r1.Id);
+        Assert.Equal(result.Id, r2.Id);
+    }
+
+    private RealEstateDto MockRealEstateData()
+    {
+        return new()
+        {
+            Size = 1000,
+            RoomNumber = 10,
+            BuildingType = "Apartment",
+            CreationTime = DateTime.Now,
+            ModifiedTime = DateTime.Now,
+        };
+    }
 }
