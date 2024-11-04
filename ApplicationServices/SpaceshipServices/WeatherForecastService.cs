3b4c01dae28a00fd9168e3b53651fcfd43798074
Opilane TTHK <andero.magi.420+schoolgit@gmail.com>
2024-11-04 11:32:05 +0200

    Weather service

---
+++ ApplicationServices/SpaceshipServices/WeatherForecastService.cs
@@ -0,0 +1,29 @@
+﻿namespace Shop.ApplicationServices.SpaceshipServices;
+
+using Accuweather;
+using Nancy.Json;
+using Shop.Core.Dto.Weather;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class WeatherForecastService
+{
+    private readonly IAccuweatherApi _accuWeather;
+
+    public WeatherForecastService()
+    {
+        string? apiKey = Environment.GetEnvironmentVariable("ACCU_WEATHER_API_KEY");
+        _accuWeather = new AccuweatherApi(apiKey, "en-us");
+    }
+
+    async Task<LocationWeatherResultDto> GetWeather(LocationWeatherResultDto dto)
+    {
+        string json = await _accuWeather.Locations.CitySearch("Tallinn");
+        List<WeatherLocationRootDto> result = new JavaScriptSerializer().Deserialize<List<WeatherLocationRootDto>>(json);
+        
+
+    }
+}
---
+++ Shop.Core/Dto/Weather/LocationWeatherResultDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Shop.Core.Dto.Weather;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LocationWeatherResultDto
+{
+    public string CityName { get; set; }
+    public string CityCode { get; set; }
+    public int Rank { get; set; }
+    
+
+}
